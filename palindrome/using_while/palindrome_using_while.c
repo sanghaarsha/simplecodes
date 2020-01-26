1aae7f70563abc16999fb2cc3980d0b202fb08cb
sanghaarsha <sanghaarsha@gmail.com>
2020-01-26 23:34:12 +0545

    code

---
+++ palindrome/using_while/palindrome_using_while.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,num,rev,sum=0,ini;
+    printf("Enter The Number:\n");
+    scanf("%d",&num);
+    ini=num;
+
+    while(num)
+    {
+        rev=num%10;
+        sum=sum*10+rev;
+        num=num/10;
+    }
+
+    printf("The Reverse Number of %d is %d\n",ini,sum);
+    if(ini==num)
+        printf("\n%d is a palindrome",ini);
+    else
+        printf("\n%d is not a palindrome",ini);
+    getch();
+}
