8c55fd87e8c210c29132d175e8ef294429bc1a1f
sanghaarsha <sanghaarsha@gmail.com>
2020-01-26 23:31:06 +0545

    code file

---
+++ palindrome/using_for/palindrome_using_for.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+void  main()
+{
+    int i,num,rev,sum=0,ini;
+    printf("Enter a number:\n");
+    scanf("%d",&num);
+    ini=num;
+
+    for(i=num;i>0;){
+        rev=i%10;
+        sum=sum*10+rev;
+        i=i/10;
+    }
+
+    printf("Reversed number: %d\n",sum);
+    if(ini==sum)
+        printf("\n%d is palindrome",ini);
+    else
+    printf("\n%d is not a palindrome",ini);
+     getch();
+}
